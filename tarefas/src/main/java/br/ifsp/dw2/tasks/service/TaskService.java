e5ec73092894737702e5c79c05e00c3f57e754ee
Elivelton Repolho <elivelton.repolho@gmail.com>
2017-09-22 11:27:14 -0300

    retirando handler

--- tarefas/src/main/java/br/ifsp/dw2/tasks/service/TaskService.java
+++ tarefas/src/main/java/br/ifsp/dw2/tasks/service/TaskService.java
@@ -1,12 +1,10 @@
 package br.ifsp.dw2.tasks.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import br.ifsp.dw2.tasks.model.Task;
 import br.ifsp.dw2.tasks.repository.Tasks;
 import br.ifsp.dw2.tasks.repository.filter.TaskFilter;
-import br.ifsp.dw2.tasks.service.exception.CRUDException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,11 +17,6 @@ public class TaskService {
 	
 	@Transactional
 	public Task save(Task task) {
-		Optional<Task> op = tasks.findByNameIgnoreCase(task.getName());
-		if (op.isPresent()) {
-			throw new CRUDException("Tarefa com este nome já cadastrado");
-		}
-		
 		return tasks.saveAndFlush(task);
 	}
 
